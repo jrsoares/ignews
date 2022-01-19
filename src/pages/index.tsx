9d2ab39f892f7a54f71f4ecf18586a7747fa678a
Jr.Soares <juniorsoaresti@gmail.com>
2022-01-19 16:00:27 -0400

    Styling page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import Head from "next/head";
 import { Header } from "../components/Header";
+import styles from "./home.module.scss";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏻 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 }

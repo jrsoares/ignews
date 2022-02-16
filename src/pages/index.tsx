4c1f03b69c42876adf623acb60afec18e0419240
Jr.Soares <juniorsoaresti@gmail.com>
2022-02-16 16:52:26 -0400

    SSG on Page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Header } from "../components/Header";
 import styles from "./home.module.scss";
 import SubscribeButton from "../components/SubscribeButton";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { stripe } from "../services/stripe";
 
 type HomeProps = {
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1IZGQqLdaOZlFjzxse0yRrNi", {
     expand: ["product"],
   });
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 Hours
   };
 };
